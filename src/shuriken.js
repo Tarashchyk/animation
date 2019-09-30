0946549939e33275d97b095f46f2adea50d1cffa
Yevhen_Tarashchyk <maze.ultramarine@gmail.com>
2019-09-30 14:50:11 +0300

    initial commit

---
+++ src/shuriken.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './shuriken.css';
+
+class Shuriken extends Component {
+  render() {
+    return <div className="block"></div>;
+  }
+}
+
+export default Shuriken;
